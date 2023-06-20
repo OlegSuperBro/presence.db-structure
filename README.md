f65bf2d5423048f38cd55a30acef0c4b56e5883c
OlegSuperBro <olegrakovic323@gmail.com>
2023-06-20 12:39:01 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,47 @@
+# About this file
+
+Presence.db is file used by game called osu!.
+
+osu! also contain [other .db files](https://github.com/ppy/osu/wiki/Legacy-database-file-structure), but there is no documentation for presence.db
+
+Presence.db contains user info displayed in F9 menu
+
+## Known information
+
+First of all, you should check [data types](https://github.com/ppy/osu/wiki/Legacy-database-file-structure#data-types) osu! uses.
+
+### Structure of Presence.db
+
+|Data type|Description|
+|-|-|
+|Int|Game version when this file created (e.g. 20230319)|
+|Int|Users count|
+|Users*|Aforementioned Users|
+
+### Users information
+
+|Data type|Description|
+|-|-|
+|Int|Online user id|
+|String|Player name|
+|???|???|
+|???|???|
+|Byte|Player status. It is split into 4 bits. First 4 bits is booleans for "is player", "is global moderator", "is supporter". One bit not used. Second 4 bits used for displaying gamemode player curently playing (not main gamemode) "0000" - std, "0010" - taiko, "0100" - ctb, "0110" - mania|
+|Float|Longitude of player|
+|Float|Latitude of player|
+|Int|Player rank in current gamemode|
+|???|???|
+|???|???|
+|???|???|
+|???|???|
+|???|???|
+|???|???|
+|Short| It's always same. Maybe means end of user record|
+
+## If you want help
+
+I already put here my **presence.db** file and it sql version (**data.db**). You can view it in an sqllite viewer (i use [this](https://inloop.github.io/sqlite-viewer/) one).
+If you find out about some data, you can create [issue](https://github.com/OlegSuperBro/presence.db-structure/issues) or create [pull request](https://github.com/OlegSuperBro/presence.db-structure/pulls)
+
+You can use your own **presence.db** file. Just copy it in this directory and run **export_presence_to_sql.py**.
+**data.db** file should appear (or changed if it already exist) in this directory
---
+++ export_presence_to_sql.py
@@ -0,0 +1,153 @@
+# shitty code but it works
+
+import struct
+import datetime
+import sqlite3
+from enum import IntFlag, Enum
+from typing import List
+
+
+def read_bool(buffer) -> bool:
+    return struct.unpack("<?", buffer.read(1))[0]
+
+
+def read_ubyte(buffer) -> int:
+    return struct.unpack("<B", buffer.read(1))[0]
+
+
+def read_ushort(buffer) -> int:
+    return struct.unpack("<H", buffer.read(2))[0]
+
+
+def read_uint(buffer) -> int:
+    return struct.unpack("<I", buffer.read(4))[0]
+
+
+def read_float(buffer) -> float:
+    return struct.unpack("<f", buffer.read(4))[0]
+
+
+def read_double(buffer) -> float:
+    return struct.unpack("<d", buffer.read(8))[0]
+
+
+def read_ulong(buffer) -> int:
+    return struct.unpack("<Q", buffer.read(8))[0]
+
+
+def read_string(buffer, encoding: str = "utf-8") -> str:
+    strlen = 0
+    strflag = read_ubyte(buffer)
+    if (strflag == 0x0b):
+        strlen = 0
+        shift = 0
+        # uleb128
+        # https://en.wikipedia.org/wiki/LEB128
+        while True:
+            byte = read_ubyte(buffer)
+            strlen |= ((byte & 0x7F) << shift)
+            if (byte & (1 << 7)) == 0:
+                break
+            shift += 7
+    return (struct.unpack("<" + str(strlen) + "s", buffer.read(strlen))[0]).decode(encoding)
+
+
+def read_datetime(buffer) -> datetime.datetime:
+    return datetime.datetime.min + datetime.timedelta(microseconds=read_ulong(buffer) / 10)
+
+
+class Status(IntFlag):
+    player = 1 << 0
+    global_moderator = 1 << 1
+    supporter = 1 << 2
+
+
+class Gamemode(Enum):
+    std = 0
+    taiko = 2
+    catch = 4
+    mania = 6
+
+
+def str_status(byte: str) -> List[bool]:
+    try:
+        tmp = repr(Status(int(byte[1], base=16)))
+        tmp = tmp.split(".")[1]
+        tmp = tmp.split(":")[0]
+        tmp = tmp.split("|")
+        return [True if "player" in tmp else False, True if "global_moderator" in tmp else False, True if "supporter" in tmp else False]
+    except IndexError:
+        return ["None", "None", "None"]
+
+
+def str_gamemode(byte: str) -> str:
+    try:
+        tmp = repr(Gamemode(int(byte[0], base=16)))
+        tmp = tmp.split(".")[1]
+        tmp = tmp.split(":")[0]
+        return tmp
+    except IndexError:
+        return "None"
+
+
+if __name__ == "__main__":
+    conn = sqlite3.connect("data.db")
+    file = open("presence.db", "rb")
+
+    read_uint(file)
+
+    conn.execute("DROP TABLE users")
+    conn.execute("""CREATE TABLE users (
+                        id INTEGER,
+                        username TEXT,
+                        byte1 TEXT,
+                        byte2 TEXT,
+                        player INTEGER,
+                        global_moderator INTEGER,
+                        supporter INTEGER,
+                        gamemode TEXT,
+                        logitude REAL,
+                        latitude REAL,
+                        rank INTEGER,
+                        byte3 TEXT,
+                        byte4 TEXT,
+                        byte5 TEXT,
+                        byte6 TEXT,
+                        byte7 TEXT,
+                        byte8 TEXT,
+                        same_bytes TEXT
+                    );""")
+    for _ in range(read_uint(file)):
+        _id = read_uint(file)
+        username = read_string(file)
+        byte1 = hex(read_ubyte(file))[2:]
+        byte2 = hex(read_ubyte(file))[2:]
+
+        tmp1_byte = read_ubyte(file)
+        tmp_byte = hex(tmp1_byte)[2:] if len(hex(tmp1_byte)[2:]) == 2 else "0" + hex(tmp1_byte)[2:]
+        # print(tmp_byte)
+        status = str_status(tmp_byte)
+        player = status[0]
+        global_moderator = status[1]
+        supporter = status[2]
+        gamemode = str_gamemode(tmp_byte)
+
+        logitude = read_float(file)
+        latitude = read_float(file)
+
+        rank = read_uint(file)
+
+        byte3 = hex(read_ubyte(file))[2:]
+        byte4 = hex(read_ubyte(file))[2:]
+        byte5 = hex(read_ubyte(file))[2:]
+        byte6 = hex(read_ubyte(file))[2:]
+        byte7 = hex(read_ubyte(file))[2:]
+        byte8 = hex(read_ubyte(file))[2:]
+
+        same_bytes = hex(read_ubyte(file))[2:] + hex(read_ubyte(file))[2:]
+
+        conn.execute("INSERT INTO users VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+                     (_id, username, byte1, byte2, player, global_moderator, supporter, gamemode,
+                      logitude, latitude, rank, byte3, byte4, byte5, byte6, byte7, byte8, same_bytes))
+    conn.commit()
+    conn.close()
